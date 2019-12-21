98253af987f4c0f36c2efa37ae797bba364d9301
Henry10260 <henry10260@gmail.com>
2019-12-21 16:15:33 -0600

    Completed Activity 10

---
+++ Desktop/Activity10/Persistence/Seed.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static void SeedData(DataContext context)
+        {
+            if(!context.Posts.Any())
+            {
+                var Posts = new List<Post>
+                {
+                    new Post {
+                    Title = "First post!",
+                    Date = DateTime.Now.AddDays(-10),
+                    Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    },
+                    new Post{
+                    Title = "This is my second post.",
+                    Date = DateTime.Now.AddDays(-7),
+                    Body = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
+                    },
+                    new Post{
+                    Title = "Another day, another post",
+                    Date = DateTime.Now.AddDays(-4),
+                    Body = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful."    
+                    }
+                };
+
+            context.Posts.AddRange(Posts);
+            context.SaveChanges();
+            }   
+        }   
+    }
+}
